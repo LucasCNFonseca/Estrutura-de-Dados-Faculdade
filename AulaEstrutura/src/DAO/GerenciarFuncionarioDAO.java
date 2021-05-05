e1a1589d27c507ed4e48b74f785a0d383cb094d9
Lucas de Castro Nunes Fonseca <lucas.cnf2@outlook.com>
2021-05-05 10:10:18 -0300

    Update GerenciarFuncionarioDAO.java

--- AulaEstrutura/src/DAO/GerenciarFuncionarioDAO.java
+++ AulaEstrutura/src/DAO/GerenciarFuncionarioDAO.java
@@ -45,10 +45,36 @@ public class GerenciarFuncionarioDAO implements ListaEnc{
         
     }
 
+    /*
+    1 - Definir variável status que será utilizada para verificação de registro (encontrado/não)
+    2 - Criar For para percorrer a lista até o final
+    3 - Condição que verifica se o nome do funcionário é igual a chave passada para consulta
+    4 - Imprimir mensagem de registro encontrado caso exista retorno positivo do comparativo se anterior
+    5 - Parar o loop de repetição
+    6 - Verificar variável status: se falsa, exibir mensagem de registro não encontrado
+    */
     @Override
     public void consultar(String nome) {
     
+        boolean status = false;
         
+        for (No no = this.primeiro; no != null; no = no.getProximoNo()) {
+            
+            if (no.getFuncionario().getNome().equals(nome)) {
+                
+                System.out.println("Registro encontrado!");
+                status = true;
+                break;
+                
+            }
+            
+        }
+        
+        if (!status) {
+            
+            System.out.println("Registro não encontrado!");
+            
+        }
         
     }
 
@@ -69,9 +95,11 @@ public class GerenciarFuncionarioDAO implements ListaEnc{
     
         for (No no = this.primeiro; no != null; no.getProximoNo()) {
             
-            
+            System.out.print(no.getFuncionario().getNome() + "->");
             
         }
+        
+        System.out.println("");
         
     }
     
